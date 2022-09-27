3f76823b53d82f5871d27cb7890bdde49a049ec6
LokeshBhoite28 <lokeshbhoite28@gmail.com>
2022-09-27 16:53:58 +0530

    '27-09'

--- src/main/java/com/app/dao/AddressRepository.java
+++
@@ -1,9 +0,0 @@
-package com.app.dao;
-
-import org.springframework.data.jpa.repository.JpaRepository;
-
-import com.app.pojos.Address;
-
-public interface AddressRepository extends JpaRepository<Address, Long>{
-
-}
